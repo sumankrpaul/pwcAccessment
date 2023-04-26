ed635357954ce3d7585410d9c8813ef6e50d72e3
smnkumarpaul <smnkumarpaul@gmail.com>
2023-04-26 23:02:36 +0530

    Navpage and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserInfoComponent },
+  { path: 'contacts', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path:'**', component: ErrorPageComponent },
+  { path: '', pathMatch:'full', redirectTo:'users'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UserInfoComponent,
+    AboutUsComponent,
+    ContactComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
